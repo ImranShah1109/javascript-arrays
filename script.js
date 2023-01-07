61bf68d4d132d6ebc6f73ea727b42e9a8f8f9858
ImranShah1109 <imranshaha41@gmail.com>
2023-01-07 13:49:13 +0530

    update in js

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
-];
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
 
-function PrintDeveloperswithMap() {
-  //Write your code here
-}
+  
+  function PrintDeveloperswithMap() {
+    //Write your code here
+    arr.map((item) => {
+        let prof = undefined;
+        prof = item?.profession;
+        if (prof == 'developer') {
+            console.log(item);
+        }
+    });
+  }
+  
+  function PrintDeveloperbyForEach() {
+    //Write your code here
+    arr.forEach((item) => {
+        let prof = undefined;
+        prof = item?.profession;
+        if (prof == 'developer') {
+            console.log(item);
+        }
+    });
+  }
+  
+  function addData() {
+    //Write your code here
+    let data = {id:4,name:"susan",age:"20",profession:"intern"};
+    arr.push(data);
+    console.log(arr);
+  }
+  
+  function removeAdmin() {
+    //Write your code here
+    arr.forEach((item,index) => {
+        let prof = undefined;
+        prof = item?.profession;
+        if(prof == 'admin'){
+            arr.splice(index,1);
+        }
+    });
+    console.log(arr);
+  }
+  
+  function concatenateArray() {
+    //Write your code here
+    let newArr = [
+        { id: 4, name: "johny", age: "23", profession: "developer" },
+        { id: 5, name: "jacky", age: "25", profession: "developer" },
+        { id: 6, name: "carry", age: "20", profession: "trainie" },
+      ];
 
-function PrintDeveloperbyForEach() {
-  //Write your code here
-}
-
-function addData() {
-  //Write your code here
-}
-
-function removeAdmin() {
-  //Write your code here
-}
-
-function concatenateArray() {
-  //Write your code here
-}
+    arr = arr.concat(newArr);
+    console.log(arr);
+  }
